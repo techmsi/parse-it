17949dd1313f63c051b35b5d840f5636cc727b37
Sophie Idromenos <techmsi@users.noreply.github.com>
2018-11-19 13:40:50 -0500

    Update parser service with parse methods

--- server/services/Parser.js
+++ server/services/Parser.js
@@ -1,8 +1,46 @@
 const Identifier = require('../models/Identifier');
 const StopWord = require('../models/StopWord');
 
+const {
+    stripPunctuation,
+    getWords,
+    buildStopWordJson,
+    buildDateWordJson,
+    buildUnknownWordJson
+} = require('./helpers');
+
 class Parser {
-    //TODO Parse through and identify all StopWords & Identifiers
+    // Parse through and identify all StopWords & Identifiers
+    constructor (query) {
+        const sentence = stripPunctuation(query);
+        this.words = getWords(sentence);
+        this.parsed = [];
+    }
+
+    updateParsed (json) {
+        this.parsed = [...this.parsed, json];
+    }
+
+    parseWords () {
+        this.words.forEach(orignalWord => {
+        const searchWord = orignalWord.toLowerCase();
+
+        const foundDateName = Identifier.findDateWord(searchWord);
+        const foundStopWord = StopWord.isStopWord(searchWord);
+
+        if (foundStopWord) {
+            this.updateParsed(buildStopWordJson(orignalWord));
+        } else if (foundDateName) {
+            const value = foundDateName.value.format('MM-DD-YYYY');
+
+            this.updateParsed(buildDateWordJson(orignalWord, value));
+        } else {
+            this.updateParsed(buildUnknownWordJson(orignalWord));
+        }
+        });
+
+        return this.parsed;
+    }
 }
 
 module.exports = Parser;
