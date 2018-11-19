a35d0fc24ed166c267edf39960499696942cabbf
Sophie Idromenos <techmsi@users.noreply.github.com>
2018-11-19 13:35:19 -0500

    Adjust router mealtimes dates & query parsing

--- server/app.js
+++ server/app.js
@@ -1,24 +1,37 @@
 const express = require('express');
 
 const app = express();
-const router = app.Router();
+const router = express.Router();
+
+const dataIdentifiers = require('./models/Identifier');
+const { MealTimes, Dates } = dataIdentifiers;
+const ParserService = require('./services/Parser');
+
+app.use(router);
 
 router.get('/', function (req, res) {
     res.send('Parse It!');
 });
 
-//TODO Fulfill endpoint to list Identifiers & Stop Words
+//Fulfill endpoint to list Identifiers & Stop Words
 router.use('/mealtimes', function (req, res) {
-    res.json({todo: 'List mealtimes'});
+    res.json({ MealTimes });
 });
 router.use('/dates', function (req, res) {
-    res.json({todo: 'List dates'});
+    res.json({ Dates });
 });
 
-//TODO Parse the query!
+//Parse the query!
 router.post('/parse', function (req, res) {
-    const query = req.param('query');
-    res.json({todo: `Parse the query string: ${query}`});
+    const { query } = req.query;
+
+    if (query) {
+        const parser = new ParserService(query);
+
+        res.json({ words: parser.parseWords() });
+    } else {
+        res.json({ message: `Unable to parse an empty query string: ${query}` });
+    }
 });
 
 
